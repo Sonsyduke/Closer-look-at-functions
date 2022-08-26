4a4ecd526a39cd6bea314298ede750e0d1b69e1a
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-26 12:44:12 +0200

    Default parameters and Values vs reference

--- script.js
+++ script.js
@@ -2,29 +2,59 @@
 
 // ===================
 // DEFAULT PARAMETERS
-const bookings = [];
+// const bookings = [];
 
-function createBooking(
-  flightNum,
-  numbPassengers = 1,
-  price = 199 * numbPassengers
-) {
-  // ES5
-  //   numbPassengers = numbPassengers || 1;
-  //   price = price || 199;
+// function createBooking(
+//   flightNum,
+//   numbPassengers = 1,
+//   price = 199 * numbPassengers
+// ) {
+// ES5
+//   numbPassengers = numbPassengers || 1;
+//   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numbPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+//   const booking = {
+//     flightNum,
+//     numbPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// }
+
+// createBooking('LH123');
+// createBooking('LH123', 8, 250);
+// createBooking('LH123', 5);
+// createBooking('LH123', 2);
+
+// createBooking('LH123', undefined, 1000);
+
+// ===================
+// VALUES VS REFERENCE
+const flight = 'LH234';
+const douglas = {
+  name: 'Douglas Lombard',
+  passport: 234563472621,
+};
+
+function checkIn(flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = `Mr. ${passenger.name}`;
+
+  if (passenger.passport === 234563472621) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
 }
 
-createBooking('LH123');
-createBooking('LH123', 8, 250);
-createBooking('LH123', 5);
-createBooking('LH123', 2);
+// checkIn(flight, douglas);
+// console.log(flight);
+// console.log(douglas);
 
-createBooking('LH123', undefined, 1000);
+function newPassport(person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+}
+
+newPassport(douglas);
+checkIn(flight, douglas);
