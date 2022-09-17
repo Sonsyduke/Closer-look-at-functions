245d36bd60c9f056f6c4ba9239f6fc3275afa4b0
Douglas Lombard <douglaslombard29@gmail.com>
2022-09-17 11:18:55 +0200

    More examples Closures

--- script.js
+++ script.js
@@ -319,19 +319,59 @@
 // ===================
 // CLOSURES
 
-function secureBooking() {
-  let passengerCount = 0;
+// function secureBooking() {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// }
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
   };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+function boardPassengers(n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
 }
 
-const booker = secureBooking();
-
-booker();
-booker();
-booker();
-
-console.dir(booker);
+boardPassengers(180, 3);
