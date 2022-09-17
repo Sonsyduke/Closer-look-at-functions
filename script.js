9053abb8c327b56ab8bc3b89647eb2385417c554
Douglas Lombard <douglaslombard29@gmail.com>
2022-09-17 11:31:50 +0200

    Coding challenge 2

--- script.js
+++ script.js
@@ -337,41 +337,57 @@
 // console.dir(booker);
 
 // Example 1
-let f;
+// let f;
 
-const g = function () {
-  const a = 23;
-  f = function () {
-    console.log(a * 2);
-  };
-};
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
 
-const h = function () {
-  const b = 777;
-  f = function () {
-    console.log(b * 2);
-  };
-};
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
 
-g();
-f();
-console.dir(f);
+// g();
+// f();
+// console.dir(f);
 
 // Re-assigning f function
-h();
-f();
-console.dir(f);
+// h();
+// f();
+// console.dir(f);
 
 // Example 2
-function boardPassengers(n, wait) {
-  const perGroup = n / 3;
+// function boardPassengers(n, wait) {
+//   const perGroup = n / 3;
 
-  setTimeout(function () {
-    console.log(`We are now boarding all ${n} passengers`);
-    console.log(`There are 3 groups, each with ${perGroup} passengers`);
-  }, wait * 1000);
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
 
-  console.log(`Will start boarding in ${wait} seconds`);
-}
+//   console.log(`Will start boarding in ${wait} seconds`);
+// }
 
-boardPassengers(180, 3);
+// const perGroup = 1000;
+// boardPassengers(180, 3);
+
+// END OF CLOSURES
+// ===================
+
+// ===================
+// CODING CHALLENGE 2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
